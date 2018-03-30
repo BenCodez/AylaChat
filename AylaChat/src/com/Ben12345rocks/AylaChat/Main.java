79fecea7eee295b96b56648b0a86299c8ed05c57
Ben12345rocks <benbergen12@gmail.com>
2018-03-30 09:46:05 -0400

    Add metrics and update checking

--- AylaChat/src/com/Ben12345rocks/AylaChat/Main.java
+++ AylaChat/src/com/Ben12345rocks/AylaChat/Main.java
@@ -2,6 +2,7 @@ package com.Ben12345rocks.AylaChat;
 
 import java.util.ArrayList;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,8 +11,10 @@ import com.Ben12345rocks.AdvancedCore.Objects.CommandHandler;
 import com.Ben12345rocks.AdvancedCore.Objects.UUID;
 import com.Ben12345rocks.AdvancedCore.Objects.User;
 import com.Ben12345rocks.AdvancedCore.Objects.UserStartup;
+import com.Ben12345rocks.AdvancedCore.Util.Metrics.BStatsMetrics;
 import com.Ben12345rocks.AdvancedCore.Util.Misc.PluginUtils;
 import com.Ben12345rocks.AdvancedCore.Util.PluginMessage.PluginMessage;
+import com.Ben12345rocks.AdvancedCore.Util.Updater.Updater;
 import com.Ben12345rocks.AylaChat.Commands.CommandLoader;
 import com.Ben12345rocks.AylaChat.Commands.Executors.CommandAylaChat;
 import com.Ben12345rocks.AylaChat.Commands.TabComplete.AylaChatTabCompleter;
@@ -24,6 +27,7 @@ public class Main extends JavaPlugin {
 
 	public static Main plugin;
 	public ArrayList<CommandHandler> commands;
+	private Updater updater;
 
 	@Override
 	public void onEnable() {
@@ -68,8 +72,48 @@ public class Main extends JavaPlugin {
 		PluginUtils.getInstance().registerCommands(plugin, "aylachat", new CommandAylaChat(plugin),
 				new AylaChatTabCompleter());
 
+		BStatsMetrics metrics = new BStatsMetrics(this);
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("channels") {
+
+			@Override
+			public String getValue() {
+				return "" + Config.getInstance().getChannels().size();
+			}
+		});
+
+		Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, new Runnable() {
+
+			@Override
+			public void run() {
+				checkUpdate();
+			}
+		}, 10L);
+
 		plugin.getLogger()
 				.info("Enabled " + plugin.getDescription().getName() + " v" + plugin.getDescription().getVersion());
+	}
+
+	public void checkUpdate() {
+		plugin.updater = new Updater(plugin, 55101, false);
+		final Updater.UpdateResult result = plugin.updater.getResult();
+		switch (result) {
+		case FAIL_SPIGOT: {
+			plugin.getLogger().info("Failed to check for update for " + plugin.getName() + "!");
+			break;
+		}
+		case NO_UPDATE: {
+			plugin.getLogger().info(plugin.getName() + " is up to date! Version: " + plugin.updater.getVersion());
+			break;
+		}
+		case UPDATE_AVAILABLE: {
+			plugin.getLogger().info(plugin.getName() + " has an update available! Your Version: "
+					+ plugin.getDescription().getVersion() + " New Version: " + plugin.updater.getVersion());
+			break;
+		}
+		default: {
+			break;
+		}
+		}
 	}
 
 	@Override
